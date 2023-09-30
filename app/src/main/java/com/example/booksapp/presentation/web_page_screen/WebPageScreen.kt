e752b271c3b51b3bbdd028f85d33e0630fdeef42
Leonid Lavrov <274852@gmail.com>
2023-09-30 11:12:44 +0300

    Fixing WebView

--- app/src/main/java/com/example/booksapp/presentation/web_page_screen/WebPageScreen.kt
+++ app/src/main/java/com/example/booksapp/presentation/web_page_screen/WebPageScreen.kt
@@ -1,7 +1,9 @@
 package com.example.booksapp.presentation.web_page_screen
 
 import android.annotation.SuppressLint
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,13 +12,19 @@ import androidx.compose.ui.viewinterop.AndroidView
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun WebPageScreen(url: String) {
-    AndroidView(
-        modifier = Modifier.fillMaxSize(),
-        factory = { context ->
-            val webView = WebView(context)
-            webView.loadUrl(url)
-            webView.settings.javaScriptEnabled = true
-            webView
-        }
-    )
+    AndroidView(modifier = Modifier.fillMaxSize(), factory = { context ->
+        val webView = WebView(context)
+        webView.webViewClient = (object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+        })
+        webView.loadUrl(url)
+        webView.settings.javaScriptEnabled = true
+        webView
+    })
 }
